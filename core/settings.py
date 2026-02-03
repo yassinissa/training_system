6e0edf3e11124b6abb426e81584062ad3d0d2693
yassinissa <yassinissa479@gmail.com>
2026-02-03 19:53:01 +0300

    Configure PostgreSQL for Render deployment and add dj-database-url

--- core/settings.py
+++ core/settings.py
@@ -108,15 +108,22 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Database
+# Use PostgreSQL on Render (production), SQLite locally
+import dj_database_url
+
+if os.environ.get('RENDER', None):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
